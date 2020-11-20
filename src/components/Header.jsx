31c6315ca720fc3258f9832e6dd378018312d7cf
Angoose <68403129+Angoooose@users.noreply.github.com>
2020-11-19 22:52:32 -0600

    Adding GitHub logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import '../styles/Header.css';
+import githubLogo from '../assets/GitHub-Mark-Light-120px-plus.png';
 
 export default function Header() {
     return (
         <div className="header raised-card">
-            Tic Tac Toe
+            tic-tac-toe
+            <a href="https://github.com/Angoooose/tic-tac-toe">
+                <img className="header-img" src={githubLogo}></img>
+            </a>
         </div>
     );
 }
